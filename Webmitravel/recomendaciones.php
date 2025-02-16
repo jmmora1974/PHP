b9cbd91d7905bf983de909a5f42667026ce07a02
Jose Miguel Mora Perez <jmmora1974@gmail.com>
2025-02-17 00:29:20 +0100

    modified:   .metadata/.log 	modified:   .metadata/.mylyn/.tasks.xml.zip 	modified:   .metadata/.mylyn/tasks.xml.zip 	modified:   .metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi 	modified:   Webmitravel/css/estilo.css 	modified:   Webmitravel/recomendaciones.php

--- Webmitravel/recomendaciones.php
+++ Webmitravel/recomendaciones.php
@@ -14,8 +14,8 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet"
 	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css"
-	href="https://robertsallent.com/css/generic.css">
+<!-- ><link rel="stylesheet" type="text/css"
+	href="https://robertsallent.com/css/generic.css">  -->
 <!-- estilo propio.-->
 <link rel="stylesheet" href="./css/estilo.css">
 <title>Mitravel</title>
