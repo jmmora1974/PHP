aad9055efece67164c241be45627228e0ff5de32
jmmora1974 <jmmora1974@gmail.com>
2025-02-20 13:54:02 +0100

    modified:   .metadata/.bak_0.log 	modified:   .metadata/.lock_info 	modified:   .metadata/.log 	modified:   .metadata/.mylyn/.tasks.xml.zip 	modified:   .metadata/.mylyn/repositories.xml.zip 	modified:   .metadata/.mylyn/tasks.xml.zip 	modified:   .metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.core.resources 	modified:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.debug.ui.prefs 	modified:   .metadata/.plugins/org.eclipse.dltk.core.index.lucene/index/.mappings 	modified:   .metadata/.plugins/org.eclipse.dltk.core/Containers.dat 	modified:   .metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi 	modified:   .metadata/.plugins/org.eclipse.php.ui/pdt-images/0.png 	modified:   .metadata/.plugins/org.eclipse.wst.sse.core/task-tags.properties 	modified:   .metadata/.plugins/org.eclipse.wst.validation/dep.index 	modified:   .metadata/version.ini 	modified:   PHP31_Ejemplo_MVC1/app/config/config.php 	new file:   PHP31_Ejemplo_MVC1/app/controllers/libro/list.php 	new file:   PHP31_Ejemplo_MVC1/app/controllers/libro/show.php 	new file:   PHP31_Ejemplo_MVC1/app/controllers/welcome.php 	modified:   PHP31_Ejemplo_MVC1/app/public/index.php 	new file:   PHP31_Ejemplo_MVC1/app/views/error.php 	new file:   PHP31_Ejemplo_MVC1/app/views/libro/detalles.php 	new file:   PHP31_Ejemplo_MVC1/app/views/libro/lista.php 	new file:   PHP31_Ejemplo_MVC1/app/views/portada.php 	new file:   UF1844- PHP Activitat 4/.buildpath 	new file:   UF1844- PHP Activitat 4/.project 	new file:   UF1844- PHP Activitat 4/.settings/org.eclipse.core.resources.prefs 	new file:   UF1844- PHP Activitat 4/.settings/org.eclipse.php.core.prefs 	new file:   UF1844- PHP Activitat 4/.settings/org.eclipse.wst.common.project.facet.core.xml 	new file:   UF1844- PHP Activitat 4/app/autoload.php 	new file:   UF1844- PHP Activitat 4/app/config/config.php 	new file:   UF1844- PHP Activitat 4/app/exceptions/DatabaseException.php 	new file:   UF1844- PHP Activitat 4/app/exceptions/NotFoundException.php 	new file:   UF1844- PHP Activitat 4/app/exceptions/SQLException.php 	new file:   UF1844- PHP Activitat 4/app/libraries/DB.php 	new file:   UF1844- PHP Activitat 4/app/libraries/DBMysqli.php 	new file:   UF1844- PHP Activitat 4/app/libraries/Model.php 	new file:   UF1844- PHP Activitat 4/app/models/Libro.php 	new file:   UF1844- PHP Activitat 4/app/models/Prestamo.php 	new file:   UF1844- PHP Activitat 4/app/models/Socio.php 	new file:   UF1844- PHP Activitat 4/app/models/Tema.php 	new file:   UF1844- PHP Activitat 4/app/public/index.php 	new file:   UF1844- PHP Activitat 4/app/test/app/Models/Libro.php 	new file:   UF1844- PHP Activitat 4/app/test/app/Models/Model.php 	new file:   UF1844- PHP Activitat 4/app/test/app/Models/Model/Model.php 	new file:   UF1844- PHP Activitat 4/app/test/app/Models/Socio.php 	new file:   UF1844- PHP Activitat 4/app/test/testLibro.php 	new file:   UF1844- PHP Activitat 4/app/test/testPrestamo.php

---
+++ PHP31_Ejemplo_MVC1/app/views/libro/detalles.php
@@ -0,0 +1,43 @@
+<!DOCTYPE>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+<meta name="description"
+		content="Lista libros PHP 31 CRUD MVC ">
+		<meta name="author" content="Jose Miguel Mora Perez">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- frameworks predefinidos..-->
+		<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		-->
+		
+		<link rel="stylesheet" type="text/css" href="https://robertsallent.com/css/generic.css">
+		<!-- estilo propio.-->
+		<link rel="stylesheet" href="./css/estilo.css">
+		<title>Listado libros de la biblioteca</title>
+		<link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+		
+		
+		</head>
+		<body>
+			<h2>Detalles del libro</h2>
+			<h3><?=$libro->titulo?></h3>
+			
+			<p><b>ISBN:</b>  	<?= $libro->isbn ?></p>
+			<p><b>Titulo:</b>  	<?= $libro->titulo ?></p>
+			<p><b>Editorial:</b>  	<?= $libro->editorial ?></p>
+			<p><b>Autor:</b>  	<?= $libro->autor ?></p>
+			<p><b>Idioma:</b>  	<?= $libro->idioma ?></p>
+			<p><b>Edicion:</b>  	<?= $libro->edicion ?></p>
+			<p><b>Edad Recomendada:</b>  	
+				<?= $libro->edadrecomendada ? $libro->edadrecomendada : 'TP' ?></p>
+				
+			<div class="centrado">
+				<a class="button" href="index.php?controlador=libro/list">Lista de libros</a>
+			</div>
+		</body>
+	</html>
+	
+	
