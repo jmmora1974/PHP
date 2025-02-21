6a026a0078af6d30263a6e8c9780a4c53c501f39
jmmora1974 <jmmora1974@gmail.com>
2025-02-21 13:59:48 +0100

    modified:   .metadata/.plugins/org.eclipse.core.resources/.root/.indexes/properties.index 	modified:   .metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.core.resources 	modified:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.debug.ui.prefs 	modified:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.ide.prefs 	modified:   .metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi 	modified:   UF1844- PHP Activitat 4/app/views/socio/actualizar.php

--- UF1844- PHP Activitat 4/app/views/socio/actualizar.php
+++ UF1844- PHP Activitat 4/app/views/socio/actualizar.php
@@ -6,36 +6,62 @@ require '../templates/template.php';
 <?php head() ?>
 
 		<body>
-			<h3>Edición de un libro: <?=$libro->titulo?></h3>
-			<form method="POST" action="index.php?controlador=libro/update">
-				<input type="hidden" name="id" value="<?=$libro->id?>">
+			<h3>Edición de un socio: <?=$socio->nombre?> <?=$socio->apellidos?></h3>
+			<form method="POST" action="index.php?controlador=socio/update">
+			
+			<input type="hidden" name="id" value="<?=$socio->id?>">
+			
+			<label for="nombre">Nombre</label>
+				<input type="text" name="nombre"  value="<?=$socio->nombre?>">
+				<br>
+			<label for="apellidos">Apellidos</label>
+				<input type="text" name="apellidos" value="<?=$socio->apellidos?>">
+				<br>
+				<label for="dni">dni</label>
+				<input type="text" name="dni" value="<?=$socio->dni?>">
+				<br>
+				<label for="nacimiento">nacimiento</label>
+				<input type="date" name="nacimiento" value="<?=$socio->nacimiento?>">
+				<br>
+				<label for="email">email</label>
+				<input type="email" name="email" value="<?=$socio->email?>">
+				<br>
+				<label for="direccion">direccion</label>
+				<input type="text" name="direccion" value="<?=$socio->direccion?>">
+				<br>
+				<label for="cp">cp</label>
+				<input type="text" name="cp" value="<?=$socio->cp?>">
+				<br>
+				<label for="poblacion">poblacion</label>
+				<input type="text" name="poblacion" value="<?=$socio->poblacion?>">
+				<br>
+				<label for="provincia">provincia</label>
+				<input type="text" name="provincia" value="<?=$socio->provincia?>">
+				<br>
+				<label for="telefono">telefono</label>
+				<input type="number"  name="telefono" value="<?=$socio->telefono?>">
+				<br>
+				<label> Sube tu imagen de perfil: </label>
+			    <br>
+			     <figure>
+			    	<img src="./imagenes/prf/default.jpg" id="preview-image" width="200" name="foto">
+			    </figure>
+			    <input type="hidden" name="MAX_FILE_SIZE" value="1240000">
+			    <input type="file" accept=".jpg, .jpeg, .gif, .png"
+			           name="fichero" id="file-with-preview"> 
+			    <br>       
+	   
+			   
 				
-				<label for="isbn">ISBN</label>
-				<input type="text" name="isbn" value="<?=$libro->isbn?>">
-				<br>
-				<label for="titulo">Título</label>
-				<input type="text" name="titulo" value="<?=$libro->titulo?>">
-				<br>
-				<label for="editorial">Editorial</label>
-				<input type="text" name="editorial" value="<?=$libro->editorial?>">
-				<br>
-				<label for="autor">Autor</label>
-				<input type="text" name="autor" value="<?=$libro->autor?>">
-				<br>
-				<label for="idioma">Idioma</label>
-				<select name="idioma">
-					<option value="Castellano" <?=$libro->idioma=='Castellano'?'selected':''?>>Castellano</option>
-					<option value="catalán" <?=$libro->idioma=='Catalán'?'selected':''?>>Catalán</option>
-					<option value="Inglés"<?=$libro->idioma=='Inglés'?'selected':''?>>Inglés</option>
-					<option value="Otros" <?=$libro->idioma=='Otros'?'selected':''?>>Otros</option>
+				<label for="conformidad">Conformidad</label>
+				<select name="conformidad" >
+					<option value="Si" <?=$socio->conformidad=='Si'?'selected':''?>>Si</option>
+					<option value="No" <?=$socio->conformidad=='No'?'selected':''?>>No</option>
 				</select>
 				<br>
-				<label for="edicion">Edicion</label>
-				<input type="number" min="0" name="edicion" value="<?=$libro->edicion?>">
-				<br>
-				<label for="edadrecomendada">Edad</label>
-				<input type="number" min="0" max="99" name="edadrecomendada" value="<?=$libro->edadrecomendada?>">
-				<br>
+				
+				
+				
 				<input type="submit" class="button" name="actualizar" value="Actualizar">	
 				<input type="reset" class="button" value="Reset">		
 			</form>
